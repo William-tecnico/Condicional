7a21c75879d2552216ce8ac29cae191ec5bf7616
William_técnico <wil.dossantos@gmail.com>
2022-02-17 13:07:51 -0300

    criando method usar ALT+enter

---
+++ src/united/united.java
@@ -0,0 +1,30 @@
+package united;
+
+public class united {
+    public static void main(String[] args) {
+
+        ifflecha();
+        ifsemflecha();
+        ifferias();
+        ifmenor();
+       
+
+
+
+
+
+
+    }
+
+    private static void ifmenor() {
+    }
+
+    private static void ifferias() {
+    }
+
+    private static void ifsemflecha() {
+    }
+
+    private static void ifflecha() {
+    }
+}
